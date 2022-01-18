7b2e239a308c71496c3b11ccc6af7a432bc248a2
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2022-01-18 16:01:50 -0300

    Update Detec 18/01

---
+++ src/screens/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from '../components/Menus/NavBar';
+import SideBar from '../components/Menus/SideBar';
+import crateGlobalStyle from '../styles/global';
+
+cosnt Home:React.FC = () => (
+    <>
+    
+      <NavBar />
+      <SideBar />
+      <GlobalStyle />
+    </>
+
+)
+
+export default Home;
---
+++ src/styles/global.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle
+      * {
+          box-sizi: border-box;
+          font-family: 'Opens Sans', sans-serif;
+      }
+      html, bady{
+          margin: 0;
+          padding: 0;
+          outline: 0;
+      }
